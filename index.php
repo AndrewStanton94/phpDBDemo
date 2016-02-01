4a96fd0426d884b6b13386500d1002cc7a7a3dea
Andrew Stanton <up683693@myport.ac.uk>
2016-02-01 00:21:42 +0000

    Started the html and js.
    Have created the page and some of the front end js.
    
    Next steps:
        1. Create ajax js. The code that will talk to the server.
        2. Create the php. Need first time set up, routing and functions to
    process the ajax requests.
    
    As I have produced similar code last year it shouldn't be taking this long.
    
    However it was clearly rushed and trying to be clever has reduced readability.
    
    Also issue of file structure. I've been having difficulties finding where functions are called.

---
+++ index.php
@@ -0,0 +1,22 @@
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<title>PHP demo</title>
+		<script type="text/javascript" charset="utf-8" src="lib/ajax.js"></script>
+		<script type="text/javascript" charset="utf-8" src="lib/main.js"></script>
+	</head>
+
+	<body>
+		<h1>phpDBDemo</h1>
+		<section>
+			<p>An example application showing connection to an SQL database using php.</p>
+
+			<p>Showing Creation and Reading</p>
+		</section>
+
+		<h2>The Data</h2>
+		<ul id="theData">
+
+		</ul>
+	</body>
+</html>

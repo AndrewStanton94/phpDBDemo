26b105fc1cc597a15831f2143c7ad76485d7b6dd
Andrew Stanton <up683693@myport.ac.uk>
2016-02-02 21:22:48 +0000

    Database functions
    I'm separating these functions from the index to keep the routing more
    readable. It may be beneficial to break this file down later.

---
+++ api/databaseFunctions.php
@@ -0,0 +1,19 @@
+<?php
+/* Very useful: http://wiki.hashphp.org/PDO_Tutorial_for_MySQL_Developers#Running_Statements_With_Parameters */
+
+	function get($db, $urlParameters){
+		/* echo $_SERVER['QUERY_STRING']; */
+		if ($_SERVER['QUERY_STRING'] == "") {
+			$query = $db->query("SELECT * FROM firstTable");
+			$result = $query->fetchAll(PDO::FETCH_ASSOC);
+		}
+		else{
+			/* print_r($urlParameters); */
+			$query = $db->prepare('SELECT * FROM firstTable WHERE id = :id ');
+			$query->bindValue(':id', $urlParameters['id'], PDO::PARAM_INT);
+			$query->execute();
+			$result = $query->fetchAll(PDO::FETCH_ASSOC);
+		}
+		echo json_encode($result);
+	}
+?>
